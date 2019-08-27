4e63ae2c37eb8bfbf5109d0a4c1f72f36ca6c591
Jeff Katz <JeffreyEricKatz@gmail.com>
2019-08-27 18:56:34 -0400

    adding beginning project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome ok man or something cool</h2>
+        </div>
+        <p className="App-intro">
+          To get on it going, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
